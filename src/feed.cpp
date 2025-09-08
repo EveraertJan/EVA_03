a9d9408689cbada8a7be3002a00f3e2a0904163f
Jan Everaert <jan@crashlab.be>
2025-09-08 21:20:36 +0200

    update fiekds

--- src/feed.cpp
+++ src/feed.cpp
@@ -170,6 +170,7 @@ post * feed::getPostOnPoint(ofVec2f lookPoint){
     
     for(int i = 0; i < posts.size(); i++) {
         ofRectangle r = posts.at(i).currentRect;
+        ofLog() << "w: " << r.width <<  " h: " << r.height <<  " l: " << lookPoint.x << "," << lookPoint.y  << " p " << posts.at(i).post_id;
         if(r.getWidth() > 0 && r.getHeight() > 0 && lookPoint.x > r.getLeft() && lookPoint.x < r.getRight() &&
            lookPoint.y > r.getTop() && lookPoint.y < r.getBottom()) {
             return &posts.at(i);
